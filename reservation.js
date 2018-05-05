0157c1407827841542b074add35eaf92449e521d
Murial Anindo <twentymurial@outlook.com>
2018-05-05 11:29:38 -0400

    first

---
+++ reservation.js
@@ -0,0 +1,8 @@
+var reservationArray={
+    customerName:" ",
+    customerPhonenumber:0,
+    customerEmail:" ",
+    customerUniqueid:0,
+};
+
+modules.export=reservationArray;
---
+++ table.js
@@ -0,0 +1,8 @@
+var tableArray={
+    customerName:" ",
+    customerPhonenumber:0,
+    customerEmail:" ",
+    customerUniqueid:0,
+};
+
+modules.export=tableArray;
